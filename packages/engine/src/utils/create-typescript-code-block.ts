1bef9a2bd9049e57d53c6b62c947344d2317415b
jliocsar <juliocesarmfo@gmail.com>
2022-05-02 08:41:12 -0300

    fix(packages/engine): fix `parseErrorMessage` errors splitting

--- packages/engine/src/utils/create-typescript-code-block.ts
+++ packages/engine/src/utils/create-typescript-code-block.ts
@@ -1,4 +1,8 @@
 export const createTypeScriptCodeblock = (code: string) => {
   const snippet = code.replace(/'/g, '')
+  console.log({
+    code,
+    snippet,
+  })
   return ['\n```ts\n', snippet, '\n```\n'].join('')
 }
--- packages/engine/src/utils/parse-error-message.ts
+++ packages/engine/src/utils/parse-error-message.ts
@@ -3,7 +3,7 @@ import { createTypeScriptCodeblock } from './create-typescript-code-block'
 const TYPESCRIPT_ERROR_CODE_SNIPPET_REGEX = /'([^']*)'/g
 
 export const parseErrorMessage = (message: string) => {
-  const errors = message.split(/\.\s*/).slice(0, -1)
+  const errors = message.replace(/\.$/, '').split(/\.\s+/)
   const parsedErrors = errors.map(error =>
     error.replace(
       TYPESCRIPT_ERROR_CODE_SNIPPET_REGEX,
